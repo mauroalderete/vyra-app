798e974d82c8d34998675560b348fc14796b48b3
Rayquen <rayquen.zero@gmail.com>
2021-01-12 21:49:11 -0300

    diseño basico

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup
+
   constructor() { }
 
   ngOnInit() {
+    let formBuilder = new FormBuilder()
+
+    this.loginForm = formBuilder.group({
+      user: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+  
+  login(){
+    
   }
 
 }
