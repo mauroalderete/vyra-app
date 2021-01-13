ec658d843b526354595552e116251e032f69bb38
Rayquen <rayquen.zero@gmail.com>
2021-01-12 22:11:34 -0300

    esquema basico de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { ISession } from 'src/app/models/session.model';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,9 @@ export class LoginPage implements OnInit {
 
   loginForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private loadingController: LoadingController
+  ) { }
 
   ngOnInit() {
     let formBuilder = new FormBuilder()
@@ -22,7 +26,28 @@ export class LoginPage implements OnInit {
   }
   
   login(){
-    
-  }
 
+    let loadingPromise = this.loadingController.create({
+      spinner: "bubbles",
+      translucent: true,
+      message: 'Iniciando sesion...',
+      backdropDismiss: true
+    })
+
+    loadingPromise.then( loading =>{
+      loading.present().then( () => {
+        console.log('ready')
+      } )
+
+      loading.onDidDismiss().then( data => {
+        console.log('dismiss: ', data)
+      })
+
+      let session: ISession = {
+        user: (this.loginForm.value.user as string),
+        password: (this.loginForm.value.password as string)
+      }
+    } )
+    console.log('login')
+  }
 }
