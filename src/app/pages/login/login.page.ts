4887fa0f6bfc0bf054009101c21f96c47c5c124f
Rayquen <rayquen.zero@gmail.com>
2021-01-12 22:40:25 -0300

    implementacion de circuito de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { AlertController, LoadingController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 import { ISession } from 'src/app/models/session.model';
+import { LoginStore } from 'src/app/stores/login.store';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +13,11 @@ import { ISession } from 'src/app/models/session.model';
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup
+  loginSubscription: Subscription = null
 
   constructor(
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private loginStore: LoginStore
   ) { }
 
   ngOnInit() {
@@ -41,12 +45,24 @@ export class LoginPage implements OnInit {
 
       loading.onDidDismiss().then( data => {
         console.log('dismiss: ', data)
+        this.loginSubscription?.unsubscribe()
       })
 
       let session: ISession = {
         user: (this.loginForm.value.user as string),
         password: (this.loginForm.value.password as string)
       }
+
+      this.loginSubscription = this.loginStore.login(session).subscribe( token => {
+        console.log('[login::login]Ok')
+        //redireccionamiento
+      }, reason => {
+        console.error('[Login::Login]', reason)
+        //mostrar error
+      }, () => {
+        console.log('[login::login]Completado')
+        //mostrar otra clase de error
+      } )
     } )
     console.log('login')
   }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { ISession } from '../models/session.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  login( session: ISession ) : Observable<string> {
+    return new Observable( observer => {
+      console.log("[LoginService::login]Start")
+
+      var headers = new HttpHeaders();
+
+      headers.append('Accept', 'application/json')
+      headers.append('Content-Type', 'application/json')
+
+      let sub = this.httpClient.post(
+        'https://vyra.herokuapp.com/api/login',
+        session,
+        {headers: headers}
+      ).subscribe( response => {
+        if ( 'token' in response ){
+          observer.next(response['token'])
+        } else {
+          observer.error("Token no generado")
+        }
+      }, reason => {
+        const httpErrorResponse: HttpErrorResponse = reason
+        switch( httpErrorResponse.status ){
+          case 400:{
+            observer.error(`Conexión interrumpida. ${httpErrorResponse.error['error']}`)
+            break
+          }
+          case 401:{
+            observer.error(`Login invalido. ${httpErrorResponse.error['error']}`)
+          }
+          default:{
+            observer.error(`Problema desconocido. Detalle del error: ${JSON.stringify(httpErrorResponse.error)}`)
+          }
+        }
+      })
+
+      return {
+        unsubscribe(){
+          console.log('[LoginService::login]Terminando')
+          sub?.unsubscribe()
+        }
+      }
+
+    } )
+  }
 }
