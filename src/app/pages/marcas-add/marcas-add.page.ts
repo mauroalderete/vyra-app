e0aca6394d00669bc3edd007024bed8402cd9555
Rayquen <rayquen.zero@gmail.com>
2021-01-16 15:56:59 -0300

    logica para guardar una nueva marca

--- src/app/pages/marcas-add/marcas-add.page.ts
+++ src/app/pages/marcas-add/marcas-add.page.ts
@@ -33,4 +33,54 @@ export class MarcasAddPage implements OnInit {
   ngOnInit() {
   }
 
+  save(){
+    let loadingControl = this.loadingController.create({
+      spinner: "bubbles",
+      translucent: true,
+      message: 'Guardando...',
+      backdropDismiss: true
+    })
+
+    loadingControl.then( loading =>{
+      loading.present().then()
+
+      loading.onDidDismiss().then( data => {
+        this.addSub?.unsubscribe()
+      })
+
+      let marca : IMarca = {
+        marca: 0,
+        nombre: this.addForm.value.nombre,
+        notas: this.addForm.value.notas
+      }
+
+      this.addSub = this.marcasStore.add( marca ).subscribe( marca => {
+        loading.dismiss().then( ()=> {
+          this.location.back()
+        })
+      }, async error => {
+        console.error('[Login::Login] ', error)
+        loading.dismiss()
+
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: error,
+          buttons: ['OK']
+        });
+    
+        await alert.present();
+      }, async () => {
+        loading.dismiss()
+
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: 'Pasó algo raro... llamar a desarrollo',
+          buttons: ['OK']
+        });
+    
+        await alert.present();
+      } )
+    } )
+
+  }
 }
