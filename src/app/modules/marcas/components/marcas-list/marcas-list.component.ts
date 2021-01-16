d06927a1cdacb170bb79fc24ffe9c3819780fecb
Rayquen <rayquen.zero@gmail.com>
2021-01-16 14:14:20 -0300

    prueba basica de litado

--- src/app/modules/marcas/components/marcas-list/marcas-list.component.ts
+++ src/app/modules/marcas/components/marcas-list/marcas-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MarcasStore } from '../../stores/marcas.store';
 
 @Component({
   selector: 'app-marcas-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarcasListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private marcasStore: MarcasStore
+  ) { }
 
   ngOnInit() {}
 
