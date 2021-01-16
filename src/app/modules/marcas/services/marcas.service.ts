3407fea2dd4603d301ef547c2e407b288ee0beec
Rayquen <rayquen.zero@gmail.com>
2021-01-16 13:39:40 -0300

    muevo los servicios a su modulo

---
+++ src/app/modules/marcas/services/marcas.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { HttpClient, HttpHeaders  } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+
+import { IMarca } from '../models/marca.model'
+import { LoginStore  } from "../../../stores/login.store";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarcasService implements OnInit, OnDestroy{
+
+  token : string
+  tokenSub: Subscription
+
+  constructor(
+    private httpClient: HttpClient,
+    private loginStore: LoginStore
+  ) { }
+
+  ngOnInit() {
+    this.tokenSub = this.loginStore.token.subscribe( token => {
+      this.token = token
+    } )
+  }
+
+  ngOnDestroy(){
+    this.tokenSub.unsubscribe()
+  }
+
+  get()
+  get(marca?: number | IMarca){
+    
+    console.log('[MarcasService::get] ', marca)
+
+    const headers = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer " + this.token
+    });
+
+    return this.httpClient.get(`${environment.API_URL}/marcas${marca? (marca['marca']? marca['marca'] : marca) : ''}`, {headers: headers})
+  }
+
+}
